82d658413d82c70941fccfe81734ec26dee35e6d
emmaagwu <emmanuelagwu89@gmail.com>
2023-09-19 11:00:26 -0700

    changed_return_value

--- 0-printf.c
+++ 0-printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 
 	if (format == NULL) /* This handles the null string and prints nothing */
 	{
-		return (0);
+		return (-1);
 	}
 
 	va_start(elements, format);
